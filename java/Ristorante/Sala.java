fedfa2ea2e6e5a617ff16208024989cc78f78d31
Diego Mignani <74722720+Diego-Mignani@users.noreply.github.com>
2020-12-31 12:42:02 +0100

    Update Sala.java

--- java/Ristorante/Sala.java
+++ java/Ristorante/Sala.java
@@ -1,10 +1,20 @@
 package Ristorante;
 import Liste.ListaOggettiPrenotabili;
-//@author DiegoMignani
-
+ /*
+ * @author DiegoMignani
+ * Classe che eredita da ListaOggettiPrenotabili.
+ * Gestisce l'elenco dei tavoli in una sala del ristorante.
+ */
 public class Sala extends ListaOggettiPrenotabili<Tavolo>{
 	
-
+	/*
+	* Metodo che gestisce l'elenco delle prenotazioni dei tavoli dentro al ristorante
+	* prendendo in input:
+	* @param la stringa della richiesta
+	* @param la stringa nome a cui si vuole intestare la prenotazione
+	* @param il numero di persone per cui si vuole prenotare
+	* @return un booleano se l'operazione è andata a buon fine o no
+	*/
 	public boolean prenota(String richiesta,String nome,int numPers){
 		for(Tavolo tavolo : super.lista) {
 			
@@ -18,16 +28,3 @@ public class Sala extends ListaOggettiPrenotabili<Tavolo>{
 	}
 	
 	}
-
-
-
-/*private int numTavoli = 0;
-	private static int prenotati = 0;
-	
-	
-	public int liberi() {return (numTavoli - prenotati);}
-	public int getMassimo() { return numTavoli; }
-	
-	public int getPrenotati() { return prenotati; }
-	*/
-
