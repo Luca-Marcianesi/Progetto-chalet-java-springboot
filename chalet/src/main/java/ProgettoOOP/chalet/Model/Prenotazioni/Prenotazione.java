6e0704af32683060996e806105911a33b8c6549d
LucaMarcia <74722742+Luca-Marcianesi@users.noreply.github.com>
2020-12-31 19:35:46 +0100

    Aggiunta commenti

--- chalet/src/main/java/ProgettoOOP/chalet/Model/Prenotazioni/Prenotazione.java
+++ chalet/src/main/java/ProgettoOOP/chalet/Model/Prenotazioni/Prenotazione.java
@@ -1,28 +1,51 @@
 package ProgettoOOP.chalet.Model.Prenotazioni;
 
+/**
+ * @author LucaMarcinesi
+ * Classe che gestisce una prenotazione
+ *
+ */
 public class Prenotazione {
 	
+	/**
+	 * Numero della prenotazione
+	 */
 	private int numeroPrenotazione;
+	/**
+	 * Nome di chi prenota
+	 */
 	private String nome;
 	
+	/**
+	 * Costruttore della classe
+	 * @param nome  Nome di chi prenota
+	 * @param numeroPrenotazione Numero della prenotazione
+	 */
 	public Prenotazione (String nome,int numeroPrenotazione) {
 		this.nome = nome;
 		this.numeroPrenotazione = numeroPrenotazione++;
 			
 	}
 	
-	
-	public String toString() {
-		return (" Numero prenotazione " + this.numeroPrenotazione + " Nome " + this.nome);
-	}
-	
+	/**
+	 * @return Restituisce il nome di chi ha prenotato
+	 */
 	public String getNome() {
 		return this.nome;
 	}
 	
+	/**
+	 * @return Ritorna il numero della prenotazione
+	 */
 	public int getNumeroPrenotazione() {
 		return this.numeroPrenotazione;
 	}
 	
+	/**
+	 *Trasforma la prenotazione in stringa
+	 */
+	public String toString() {
+		return (" Numero prenotazione " + this.numeroPrenotazione + " Nome " + this.nome);
+	}
 
 }
