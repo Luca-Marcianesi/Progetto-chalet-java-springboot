9399ea10703d6e2bbd52c9637627614529955194
Edoardo-Tarulli <75034582+Edoardo-Tarulli@users.noreply.github.com>
2021-01-01 19:50:59 +0100

    Classe Sala aggiornata con commenti al 01/01
    Classe Sala

--- chalet/src/main/java/ProgettoOOP/chalet/Model/Ristorante/Sala.java
+++ chalet/src/main/java/ProgettoOOP/chalet/Model/Ristorante/Sala.java
@@ -1,11 +1,20 @@
 package ProgettoOOP.chalet.Model.Ristorante;
 
 import ProgettoOOP.chalet.Model.Liste.ListaOggettiPrenotabili;
+
 //@author DiegoMignani
+//@author Edoardo Tarulli
+//@author Marcianesi Luca
 
 public class Sala extends ListaOggettiPrenotabili<Tavolo>{
 	
-
+	/**
+	 * Metodo che prende in ingresso i parametri richiesti per una prenotazione del tavolo cioè
+	 * richiesta(tavolo), nome e numero delle persone. Il metodo cerca se esiste un oggetto tavolo
+	 * con il numero di posti necessari. Se è disponibile lo prenota e aggiunge un oggetto prenotazione
+	 * e restituisce true. Se la prenotazione invece non è stato possibile effettuarla, restituisce false.
+	 */
+	
 	public boolean prenota(String richiesta,String nome,int numPers){
 		for(Tavolo tavolo : super.lista) {
 			
@@ -14,12 +23,8 @@ public class Sala extends ListaOggettiPrenotabili<Tavolo>{
 				tavolo.aggiungiPrenotazione(nome);
 				return true;
 			}	
-	}
+	  }
 	return false;
 	}
 	
-	}
-
-
-
-
+}
