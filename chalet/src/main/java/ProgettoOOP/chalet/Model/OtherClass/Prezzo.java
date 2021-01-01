75051cefdbd18cebcc5325982d79eaccb133ba1b
Edoardo-Tarulli <75034582+Edoardo-Tarulli@users.noreply.github.com>
2021-01-01 19:28:22 +0100

    Classe Prezzo aggiornamento con commenti 01/01
    Classe Prezzo

--- chalet/src/main/java/ProgettoOOP/chalet/Model/OtherClass/Prezzo.java
+++ chalet/src/main/java/ProgettoOOP/chalet/Model/OtherClass/Prezzo.java
@@ -1,31 +1,44 @@
 package ProgettoOOP.chalet.Model.OtherClass;
 
+/**
+ * @author Edoardo Tarulli
+ *
+ */
+
 public class Prezzo {
+	
+	/**
+	 * @param prezzo --> variabile "prezzo" di tipo float.
+	 */
+	private float prezzo;
+	
+	/** Costruttore della classe Prezzo che prende in ingresso, come parametro, il prezzo.
+	 */
+	public Prezzo(float prezzo) {
 		
-		
-		private float prezzo;
-		
-		public Prezzo(float prezzo) {
-			this.prezzo = prezzo;
-		}
-		
-		public void setPrezzo ( float prezzo ) {  
-			this.prezzo = prezzo;
-		}
-		
-		
-		public float getPrezzo () {
-			return this.prezzo;
-		}
-		
-		public String toString () {
-			return ("Prezzo :" + prezzo );
-		}
-		
-		
-		
+		this.prezzo = prezzo;
 	}
-
-
-
-
+	
+	/**
+	 * Metodo utile per andare a specificare il valore del prezzo.
+	 */
+	public void setPrezzo ( float prezzo ) {  
+		this.prezzo = prezzo;
+	}
+	
+	/**
+	 * Metodo get che restituisce il valore del prezzo.
+	 */
+	public float getPrezzo () {
+		
+		return this.prezzo;
+	}
+	
+	/**
+	 * Metodo che restituisce il valore del prezzo trasformato in stringa.
+	 */
+	public String toString () {
+		
+		return ("Prezzo :" + prezzo );
+	}
+}
