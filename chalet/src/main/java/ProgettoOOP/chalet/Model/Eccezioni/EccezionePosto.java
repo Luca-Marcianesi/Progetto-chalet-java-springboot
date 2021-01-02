b2137a0abc8b5822056bdd28cb8b4d693445ea98
LucaMarcia <74722742+Luca-Marcianesi@users.noreply.github.com>
2021-01-02 16:41:02 +0100

    Eccezioni
    2/1

---
+++ chalet/src/main/java/ProgettoOOP/chalet/Model/Eccezioni/EccezionePosto.java
@@ -0,0 +1,7 @@
+package ProgettoOOP.chalet.Model.Eccezioni;
+
+public class EccezionePosto extends Exception {
+	public EccezionePosto() {
+		super("Posto non accetabile");
+	}
+}
---
+++ chalet/src/main/java/ProgettoOOP/chalet/Model/Eccezioni/EccezionePrezzo.java
@@ -0,0 +1,9 @@
+package ProgettoOOP.chalet.Model.Eccezioni;
+
+public class EccezionePrezzo extends Exception{
+	
+	public EccezionePrezzo() {
+		super("Prezzo non accettabile");
+	}
+
+}
