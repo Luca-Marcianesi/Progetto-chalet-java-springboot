d90509285da356f0bf701d0cddf7249fe94ee6fa
Edoardo-Tarulli <75034582+Edoardo-Tarulli@users.noreply.github.com>
2021-01-02 18:25:59 +0100

    Update EccezioneSpiaggia.java

--- chalet/src/main/java/ProgettoOOP/chalet/Model/Eccezioni/EccezioneSpiaggia.java
+++ chalet/src/main/java/ProgettoOOP/chalet/Model/Eccezioni/EccezioneSpiaggia.java
@@ -3,6 +3,6 @@ package ProgettoOOP.chalet.Model.Eccezioni;
 public class EccezioneSpiaggia extends Exception{
 	
 	public EccezioneSpiaggia() {
-		super("Prezzo non creata");
+		super("Spiaggia non creata");
 	}
 }
