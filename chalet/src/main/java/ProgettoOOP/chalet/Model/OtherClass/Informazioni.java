b688173852c51657b5111620ff27821c95b2bd54
LucaMarcia <74722742+Luca-Marcianesi@users.noreply.github.com>
2021-01-03 15:32:12 +0100

    Update Informazioni.java
    3/1

--- chalet/src/main/java/ProgettoOOP/chalet/Model/OtherClass/Informazioni.java
+++ chalet/src/main/java/ProgettoOOP/chalet/Model/OtherClass/Informazioni.java
@@ -4,8 +4,8 @@ import java.time.LocalDate;
 import ProgettoOOP.chalet.Model.Liste.ListaOggettiConValore;
 
 /**
- * @author Luca
- *
+ * @author LucaMarcianesi
+ *Gestisce le informaazioni dello chalet
  */
 public   class Informazioni {
 	
