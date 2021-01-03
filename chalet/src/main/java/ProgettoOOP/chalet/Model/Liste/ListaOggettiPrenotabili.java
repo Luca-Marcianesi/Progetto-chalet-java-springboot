21c2e52d6322f9f97eff766592969191da372245
Edoardo-Tarulli <75034582+Edoardo-Tarulli@users.noreply.github.com>
2021-01-03 16:26:34 +0100

    Update ListaOggettiPrenotabili.java

--- chalet/src/main/java/ProgettoOOP/chalet/Model/Liste/ListaOggettiPrenotabili.java
+++ chalet/src/main/java/ProgettoOOP/chalet/Model/Liste/ListaOggettiPrenotabili.java
@@ -3,7 +3,7 @@ package ProgettoOOP.chalet.Model.Liste;
 import ProgettoOOP.chalet.Model.Interfacce.OggettoPrenotabile;
 
 /**
- * @author LucaMarcianesi
+ * @author Luca Marcianesi
  * Classe che estende la piÃƒÂ¹ generale lista e si occupa delle operazioni
  * utili sulle liste di oggettoPrenotabile
  *
@@ -14,9 +14,9 @@ public class ListaOggettiPrenotabili<T extends OggettoPrenotabile>  extends List
 	/**
 	 * Permette di prenotare un certo oggetto a partire dal nome dell'oggetto(richiesta) 
 	 * e dal nome di chi fa la prenotazione (nome)
-	 * Scorre la lista di oggetti e fa due verifiche:1 se esiste un oggetto con lo stesso nome della 
-	 * richiesta e se lo trova se quel oggetto ÃƒÂ¨ disponibile
-	 * Se lo trova ed è disponibile modifica lo stato dell'oggetto e aggiune la prenotazione all'oggetto
+	 * Scorre la lista di oggetti e fa due verifiche: 1) se esiste un oggetto con lo stesso nome della 
+	 * richiesta. 2) Verifica se quell'oggetto è disponibile.
+	 * Se lo trova ed è disponibile modifica lo stato dell'oggetto e aggiunge la prenotazione all'oggetto
 	 * @param richiesta nome dell'oggetto
 	 * @param nome nome di chi fa la prenotazione
 	 * @return ritorna vero se la prenotazione è andata a buon fine altrimenti torna false
@@ -32,8 +32,5 @@ public class ListaOggettiPrenotabili<T extends OggettoPrenotabile>  extends List
 	}
 	return false;
 	}
-	
-	
-	
 	
 }
