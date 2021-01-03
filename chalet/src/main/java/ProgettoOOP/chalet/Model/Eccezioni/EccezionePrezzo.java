53a2c37d56d937a52794f7cd08a79f28c534be3b
Edoardo-Tarulli <75034582+Edoardo-Tarulli@users.noreply.github.com>
2021-01-03 16:38:39 +0100

    Update EccezionePrezzo.java

--- chalet/src/main/java/ProgettoOOP/chalet/Model/Eccezioni/EccezionePrezzo.java
+++ chalet/src/main/java/ProgettoOOP/chalet/Model/Eccezioni/EccezionePrezzo.java
@@ -1,7 +1,7 @@
 package ProgettoOOP.chalet.Model.Eccezioni;
 
 public class EccezionePrezzo extends Exception{
-	
+	private static final long serialVersionUID = 1L;
 	public EccezionePrezzo() {
 		super("Prezzo non accettabile");
 	}
