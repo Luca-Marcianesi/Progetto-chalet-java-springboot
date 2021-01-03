0eb6a266ee2c11d4bb604aa4cfec9da3b6c8c0fd
Edoardo-Tarulli <75034582+Edoardo-Tarulli@users.noreply.github.com>
2021-01-03 16:37:43 +0100

    Update EccezionePosto.java

--- chalet/src/main/java/ProgettoOOP/chalet/Model/Eccezioni/EccezionePosto.java
+++ chalet/src/main/java/ProgettoOOP/chalet/Model/Eccezioni/EccezionePosto.java
@@ -1,6 +1,7 @@
 package ProgettoOOP.chalet.Model.Eccezioni;
 
 public class EccezionePosto extends Exception {
+	private static final long serialVersionUID = 1L;
 	public EccezionePosto() {
 		super("Posto non accetabile");
 	}
